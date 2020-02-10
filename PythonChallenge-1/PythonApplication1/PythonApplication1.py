cdc0901635e264485fded1a211a09c1c9a25e4ce
Daniel Brandão <50630500+dbrandao85@users.noreply.github.com>
2020-02-10 17:30:48 -0300

    Exercicio 0 feito

---
+++ PythonChallenge-1/PythonApplication1/PythonApplication1.py
@@ -0,0 +1,5 @@
+frase = "g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj. "
+resultado = ""
+for val in frase:
+    resultado = resultado + val
+print(resultado)
