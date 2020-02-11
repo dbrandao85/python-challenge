4b82330b68afdc2df66cf6707713a7ff018b084f
Daniel Brandão <50630500+dbrandao85@users.noreply.github.com>
2020-02-11 16:14:20 -0300

    challenge 4 terminado

--- PythonChallenge-3/PythonChallenge-3/PythonChallenge_3.py
+++ PythonChallenge-3/PythonChallenge-3/PythonChallenge_3.py
@@ -1,6 +1,6 @@
 import re
 arquivo = open('arquivo.txt')
 texto = arquivo.read()
-p = re.compile('[a-z][A-Z][A-Z][A-Z][a-z][A-Z][A-Z][A-Z][a-z]')
+p = re.compile('[a-z][A-Z][A-Z][A-Z]([a-z])[A-Z][A-Z][A-Z][a-z]')
 print(p.findall(texto))
 arquivo.close()
---
+++ PythonChallenge-4/PythonChallenge-4/PythonChallenge_4.py
@@ -0,0 +1,16 @@
+import urllib.request
+import re
+#valor= '12345'
+#valor= '8022'
+valor ='63579'
+i = 1
+while i < 400:
+    with urllib.request.urlopen('http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing='+valor) as response:
+        print(i)
+        html = response.read()
+        print(html)
+        numeros = re.compile('[0-9]+')
+        temporario = numeros.findall(html.decode("utf-8"))
+        valor = temporario[0]
+        print(valor)
+        i += 1
